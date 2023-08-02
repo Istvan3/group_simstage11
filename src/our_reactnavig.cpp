f634389e5a9db86840fcb39576bd569bd77f9cb7
Istvan3 <nagy_isti47@yahoo.com>
2023-08-02 15:22:31 +0100

    Made changes in the our_reactnavig

--- src/our_reactnavig.cpp
+++ src/our_reactnavig.cpp
@@ -1,13 +1,20 @@
-// "our_reactnavig" node: subscribes laser data and publishes velocity commands
-
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
 #include "geometry_msgs/Twist.h"
 
 double obstacle_distance;
+bool obstacle_detected = false;
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
-  obstacle_distance = *std::min_element (msg->ranges.begin(), msg->ranges.begin());
+  // Find the minimum distance to the obstacle
+  obstacle_distance = *std::min_element(msg->ranges.begin(), msg->ranges.end());
+  
+  // Check if any obstacle is too close (e.g., within 0.5 meters)
+  if (obstacle_distance < 0.5) {
+    obstacle_detected = true;
+  } else {
+    obstacle_detected = false;
+  }
 }
 
 int main(int argc, char **argv){
@@ -16,28 +23,31 @@ int main(int argc, char **argv){
   
   ros::NodeHandle n;
 
-  //Publisher for /cmd_vel
+  // Publisher for /cmd_vel
   ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 100);
-  //Subscriber for /base_scan
+  // Subscriber for /base_scan
   ros::Subscriber laser_sub = n.subscribe("base_scan", 100, laserCallback);
 
-  ros::Rate loop_rate(10); //10 Hz
+  ros::Rate loop_rate(10); // 10 Hz
 
-  //initializations:
+  // Initializations
   geometry_msgs::Twist cmd_vel_msg;
+  cmd_vel_msg.linear.x = 0.5;   // Set initial linear velocity
+  cmd_vel_msg.angular.z = 0.0;  // Set initial angular velocity
 
   while (ros::ok()){
     
-      if(obstacle_distance < 1.0){
-          cmd_vel_msg.linear.x = 0.0;
-          cmd_vel_msg.angular.z = 0.0;
-          
-      }else{
-            cmd_vel_msg.linear.x = 0.5;
-            cmd_vel_msg.angular.z = 0.3;
-      }
+    if (obstacle_detected) {
+      // If an obstacle is detected, turn away from it
+      cmd_vel_msg.linear.x = 0.0;
+      cmd_vel_msg.angular.z = 0.3; // Adjust the angular velocity as needed
+    } else {
+      // If no obstacle is detected, move forward
+      cmd_vel_msg.linear.x = 0.5; // Adjust the linear velocity as needed
+      cmd_vel_msg.angular.z = 0.0;
+    }
 
-    //publish velocity commands:
+    // Publish velocity commands
     cmd_vel_pub.publish(cmd_vel_msg);
 
     ros::spinOnce();
@@ -46,4 +56,5 @@ int main(int argc, char **argv){
   }
 
   return 0;
-}
+} 
+
