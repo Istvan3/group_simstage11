776d175785e480ce1f7a66210232490d0892ecce
Istvan3 <nagy_isti47@yahoo.com>
2023-08-10 17:06:12 +0100

    Small changes has been made

--- src/our_reactnavig.cpp
+++ src/our_reactnavig.cpp
@@ -1,54 +1,53 @@
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
 #include "geometry_msgs/Twist.h"
-#include <random> // Include the random library
 
 double obstacle_distance;
 
-void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
-  obstacle_distance = *std::min_element(msg->ranges.begin(), msg->ranges.end());
+double getTurnAngularVelocity() {
+    // Set a fixed angular velocity for turning
+    // Positive value for turning left, negative value for turning right
+    static double turn_velocity = 0.25; // Adjust this value to control the turning speed
+    return turn_velocity;
 }
 
-double getRandomAngularVelocity() {
-    // Generate a random angular velocity between -1.0 and 1.0
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    static std::uniform_real_distribution<> dis(-3.0, 3.0);
-    return dis(gen);
+void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
+    obstacle_distance = *std::min_element(msg->ranges.begin(), msg->ranges.end());
 }
 
 int main(int argc, char **argv){
-  ros::init(argc, argv, "our_reactnavig");
-  ros::NodeHandle n;
+    ros::init(argc, argv, "our_reactnavig");
+    ros::NodeHandle n;
 
-  // Publisher for /cmd_vel
-  ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 100);
-  // Subscriber for /base_scan
-  ros::Subscriber laser_sub = n.subscribe("base_scan", 100, laserCallback);
+    // Publisher for /cmd_vel
+    ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 100);
+    // Subscriber for /base_scan
+    ros::Subscriber laser_sub = n.subscribe("base_scan", 100, laserCallback);
 
-  ros::Rate loop_rate(10); // 10 Hz
+    ros::Rate loop_rate(10); // 10 Hz
 
-  // Initializations:
-  geometry_msgs::Twist cmd_vel_msg;
+    // Initializations:
+    geometry_msgs::Twist cmd_vel_msg;
 
-  while (ros::ok()){
-    if (obstacle_distance < 0.8){
-      // When an obstacle is detected, stop and set random angular velocity
-      cmd_vel_msg.linear.x = 0.0;
-      cmd_vel_msg.angular.z = getRandomAngularVelocity(); // Set random angular velocity
-    } else {
-      // When no obstacle, move forward with a constant linear velocity
-      cmd_vel_msg.linear.x = 0.4;
-      cmd_vel_msg.angular.z = 0.0; // Set random angular velocity
+    while (ros::ok()) {
+        if (obstacle_distance < 0.8) {
+            // When an obstacle is detected, stop and set a larger angular velocity for turning
+            cmd_vel_msg.linear.x = 0.0;
+            cmd_vel_msg.angular.z = getTurnAngularVelocity(); // Set fixed angular velocity for turning
+        } else {
+            // When no obstacle, move forward with an increased linear velocity
+            cmd_vel_msg.linear.x = 0.8; // Increase this value for faster linear velocity
+            cmd_vel_msg.angular.z = 0.0;
+        }
+
+        // Publish velocity commands:
+        cmd_vel_pub.publish(cmd_vel_msg);
+
+        ros::spinOnce();
+
+        loop_rate.sleep();
     }
 
-   // Publish velocity commands:
-    cmd_vel_pub.publish(cmd_vel_msg);
-
-    ros::spinOnce();
-
-    loop_rate.sleep();
-  }
-
-  return 0;
+    return 0;
 }
+
